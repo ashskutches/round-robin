5898e84c3f6b26f422a04afb43dd6b85f27ac2fe
Ash Skutches <ashskutches@gmail.com>
2014-11-24 14:08:09 -0800

    Menu works and bracket is generated

--- round-robin.rb
+++ round-robin.rb
@@ -1,20 +1,3 @@
-class Player
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
-
-class Match
-  attr_accessor :player1_name, :player2_name
-
-  def initialize(p1, p2)
-    @player1_name = p1
-    @player2_name = p2
-  end
-end
-
 class Tournament
   attr_accessor :players, :matches
 
@@ -23,73 +6,95 @@ class Tournament
     @matches = matches
   end
 
-  def get_player_names
-    100.times { puts "\n" }
-    name = ""
-    while !name.include?("done") do
-      self.players << Player.new(name) unless name.empty?
-      if !self.players.empty?
-        puts "Type DONE when completed"
-        puts "\n"
-        puts "================="
-        puts "PLAYERS #{self.players.count}: #{self.players.collect(&:name).to_s.gsub(/\\n/, "")}"
-        puts "\n"
-      end
-      puts "Enter player#{self.players.size + 1}'s name"
-      puts "================="
-      name = gets
-      100.times { puts "\n" }
-    end
-  end
-
   def display_menu
+    100.times { puts "\n" }
     puts "    Menu"
     puts "========"
-    puts "1. See Players"
+    puts "1. Player Menu"
     puts "2. See Bracket"
     puts "Please select:"
     menu_option = gets
     case menu_option
     when /1/
       display_players
-      self.display_menu
+      prompt_to_add_players
+      generate_bracket
     when /2/
-      puts "TWO"
+      if @matches.size < 2
+        puts "Not enough players"
+      else
+        display_bracket
+      end
     else
       puts "Not a valid option"
     end
+    self.display_menu
   end
 
   def display_players
     100.times { puts "\n" }
     puts "Players"
     puts "=== #{@players.count} ===="
-    puts @players.collect(&:name)
+    @players.each do |player|
+      puts "#{player.id}: #{player.name}"
+    end
+  end
+
+  def display_bracket
+    @matches.each do |match|
+      puts "#{match.player1.name} vs #{match.player2.name}"
+    end
+    gets
+  end
+
+  def prompt_to_add_players
+    index = -1
+    name = ""
+    while !name.include?("done") do
+      @players << Player.new(name, index) unless name.empty?
+
+      100.times { puts "\n" }
+      puts "Type done  when completed"
+      puts "\n"
+      puts "================="
+      puts "PLAYERS #{@players.count}: #{@players.collect(&:name).to_s.gsub(/\\n/, "")}"
+      puts "\n"
+      puts "Enter player#{@players.size + 1}'s name"
+      puts "================="
+      name = gets
+      index += 1
+    end
   end
 
   def generate_bracket
-    if @players.count > 2
-      group_one = @players.take(@players.count / 2)
-      group_two = @players - group_one
-      group_one.each do |player1|
-        group_two.each do |player2|
-          @matches << Match.new(player1, player2)
-        end
-      end
-    else
-      puts "Not enough players supplied!" and return
+    @matches = []
+    @players.each do |player1|
+      (@players - [player1]).each { |player2| @matches << Match.new(player1, player2) }
     end
+  end
+end # Tournament
+
+class Player
+  attr_accessor :name, :id
+
+ def initialize(name, index)
+    @name = name
+    @id = index
+  end
+end
+
+class Match
+  attr_accessor :player1, :player2
+
+  def initialize(p1, p2)
+    @player1 = p1
+    @player2 = p2
   end
 end
 
 
+
 # Input Script
 tournament = Tournament.new
-tournament.get_player_names
-
-
-
-tournament.generate_bracket
 tournament.display_menu
-#name = gets
 
